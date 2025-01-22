bc06462c63777804be554ed29f9c8ce9e076fdf0
Andrey Ivanov <ai3501669@gmail.com>
2025-01-22 07:49:35 +0300

    models: create users.py

--- src/models/users.py
+++ src/models/users.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from database import Base
 
 
-class RolesORM(Base):
+class UsersORM(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
