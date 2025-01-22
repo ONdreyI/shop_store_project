a64f0cf409b0e8938fd05204bbce579a9fcbc4a0
Andrey Ivanov <ai3501669@gmail.com>
2025-01-22 08:46:10 +0300

    models: some changes

--- src/models/customers.py
+++ src/models/customers.py
@@ -6,6 +6,7 @@ from database import Base
 
 class CustomersORM(Base):
     __tablename__ = "customers"
+
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     first_name: Mapped[str] = mapped_column(String(200), index=True)
     last_name: Mapped[str] = mapped_column(String(200), index=True)
--- src/models/regions.py
+++ src/models/regions.py
@@ -5,7 +5,7 @@ from database import Base
 
 
 class RegionsORM(Base):
-    __tablename__ = 'regions'
+    __tablename__ = "regions"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(200), index=True)
