4148e82c9ce5cccd058c1ced5e51f692ac09e9c8
Andrey Ivanov <ai3501669@gmail.com>
2025-01-22 16:14:33 +0300

    migrations: before migrations

---
+++ src/models/__init__.py
@@ -0,0 +1,24 @@
+from .categories import CategoriesORM
+from .customers import CustomersORM
+from .managers import ManagersORM
+from .orders import OrdersORM
+from .products import ProductsORM
+from .products_with_services import ProductsWithServicesORM
+from .regions import RegionsORM
+from .roles import RolesORM
+from .services import ServicesORM
+from .users import UsersORM
+
+
+__all__ = [
+    "CategoriesORM",
+    "CustomersORM",
+    "ManagersORM",
+    "OrdersORM",
+    "ProductsORM",
+    "ProductsWithServicesORM",
+    "RegionsORM",
+    "RolesORM",
+    "ServicesORM",
+    "UsersORM",
+]
