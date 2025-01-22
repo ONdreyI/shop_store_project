5c6be9dd3795bb1de2441d433cd288b20399853d
Andrey Ivanov <ai3501669@gmail.com>
2025-01-22 08:03:57 +0300

    models: managers.py

--- src/models/managers.py
+++ src/models/managers.py
@@ -5,7 +5,7 @@ from database import Base
 
 
 class ManagersORM(Base):
-    __tablename__ = 'managers'
+    __tablename__ = "managers"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     first_name: Mapped[str] = mapped_column(String(200), index=True)
