81ff606aaa3aeb31a8f52a4a3d3f85150ac7ca57
Andrey Ivanov <ai3501669@gmail.com>
2025-01-24 21:50:28 +0300

    schemas: add patch

--- src/schemas/services.py
+++ src/schemas/services.py
@@ -8,3 +8,12 @@ class ServicesAdd(BaseModel):
 
 class Services(ServicesAdd):
     id: int
+
+    class Config:
+        orm_mode = True
+        from_attributes = True
+
+
+class ServicesPatch(BaseModel):
+    name: str | None = Field(None, max_length=200)
+    price: condecimal(max_digits=10, decimal_places=2) | None = None
