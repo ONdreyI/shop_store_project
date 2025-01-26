99dcbccdfa5e19461b8ed66afbd4ccb0823774f4
Andrey Ivanov <ai3501669@gmail.com>
2025-01-27 00:09:32 +0300

    repository: base add methods

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,4 +1,5 @@
-from sqlalchemy import select
+from pydantic import BaseModel
+from sqlalchemy import select, delete, insert, update
 
 from database import async_session_maker
 
@@ -13,3 +14,30 @@ class BaseRepository:
         query = select(self.model)
         result = await self.session.execute(query)
         return result.scalars().all()
+
+    async def get_one_ore_none(self, **filter_by):
+        query = select(self.model).filter_by(**filter_by)
+        result = await self.session.execute(query)
+        return result.scalars().one_or_none()
+
+    async def add(self, data: BaseModel):
+        add_data_stmt = (
+            insert(self.model).values(**data.model_dump()).returning(self.model)
+        )
+        result = await self.session.execute(add_data_stmt)
+        result.scalars().one()
+        await self.session.commit()
+
+    async def edit(self, data: BaseModel, exclude_unset: bool = False, **filter_by):
+        update_stmt = (
+            update(self.model)
+            .filter_by(**filter_by)
+            .values(**data.model_dump(exclude_unset=exclude_unset))
+        )
+        await self.session.execute(update_stmt)
+        await self.session.commit()
+
+    async def delete_one(self, **filter_by) -> None:
+        delete_stmt = delete(self.model).filter_by(**filter_by)
+        await self.session.execute(delete_stmt)
+        await self.session.commit()
