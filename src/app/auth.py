3a53bdbb1b83bb1c4f1eeaf25a16667b6c7e84ca
Andrey Ivanov <ai3501669@gmail.com>
2025-01-30 19:07:09 +0300

    auth: create modul for clearing code

--- src/app/auth.py
+++ src/app/auth.py
@@ -1,44 +1,26 @@
-from datetime import datetime, timezone, timedelta
+import logging
 
-import jwt
-from fastapi import APIRouter, HTTPException
-from passlib.context import CryptContext
+from fastapi import APIRouter, HTTPException, Response, Request
 from pydantic import ValidationError
 
-from src.config import settings
+from app.dependencies import UserIdDep
 from src.database import async_session_maker
 from src.repositories.users import UsersRepository
 from src.schemas.users import UserRequestAdd, UserAdd, UserLogin
+from src.services.auth import AuthService
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 router = APIRouter(
     prefix="/auth",
     tags=["Авторизация и аутентификация"],
 )
 
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(
-            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
-        )
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(
-        to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM
-    )
-    return encoded_jwt
-
-
-@router.post("/register", name="Авторизация")
+@router.post("/register", name="Аутентификация")
 async def register_user(data: UserRequestAdd):
-    hashed_password = pwd_context.hash(data.password)
+    hashed_password = AuthService().pwd_context.hash(data.password)
     new_user_data = UserAdd(
         username=data.username,
         email=data.email,
@@ -54,19 +36,41 @@ async def register_user(data: UserRequestAdd):
             raise HTTPException(status_code=401, detail=e.error())
 
 
-@router.post("/login")
+@router.post("/login", name="Авторизация")
 async def login_user(
     data: UserLogin,
+    response: Response,
 ):
     async with async_session_maker() as session:
+        logger.info(f"Searching for user with email: {data.email}")
         user = await UsersRepository(session).get_user_with_hashed_password(
             email=data.email
         )
+        logger.info(f"User found: {user}")
         if not user:
             raise HTTPException(
                 status_code=401, detail="Пользователь с таким email не зарегистрирован"
             )
-        if not verify_password(data.password, user.hashed_password):
+        if not AuthService().verify_password(data.password, user.hashed_password):
             raise HTTPException(status_code=401, detail="Пароль неверный")
-        access_token = create_access_token({"user_id": user.id})
+        access_token = AuthService().create_access_token({"user_id": user.id})
+        response.set_cookie("access_token", access_token)
         return {"access_token": access_token}
+
+
+@router.get("/me", name="Получение данных о пользователе")
+async def only_auth(user_id: UserIdDep):
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_one_ore_none(id=user_id)
+        if not user:
+            raise HTTPException(status_code=401, detail="Пользователь не авторизован")
+        return {"status": "OK", "user": user}
+
+
+@router.post(
+    "/logout",
+    name="Выход из системы!",
+)
+async def logout_user(response: Response):
+    response.delete_cookie("access_token")
+    return {"status": "OK", "details": "Выход из системы успешен"}
