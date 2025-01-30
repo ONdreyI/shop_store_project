9724073822f42f236f17f861af22f423e96ca3ab
Andrey Ivanov <ai3501669@gmail.com>
2025-01-30 19:03:26 +0300

    dep: create userId dependency

---
+++ src/app/dependencies.py
@@ -0,0 +1,18 @@
+from typing import Annotated
+
+from fastapi import Request, HTTPException, Depends
+from src.services.auth import AuthService
+
+
+def get_token(request: Request):
+    token = request.cookies.get("access_token", None)
+    if token is None:
+        raise HTTPException(status_code=401, detail="Вы не предоставили токен доступа")
+
+
+def get_current_user_id(token: str = Depends(get_token)) -> int:
+    data = AuthService().decode_jwt_token(token)
+    return data["user_id"]
+
+
+UserIdDep = Annotated[int, Depends(get_current_user_id)]
