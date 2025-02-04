05d0f6adc8e894c59e9be2d4b0d2d3866a53a012
Andrey Ivanov <ai3501669@gmail.com>
2025-02-04 08:33:19 +0300

    repository: add new method get_products_with_categories.

--- src/schemas/products.py
+++ src/schemas/products.py
@@ -22,5 +22,5 @@ class ProductsPatch(BaseModel):
 class ProductWithCategoryResponse(BaseModel):
     product_name: str = Field(..., alias="product_name")
     category_name: str = Field(..., alias="category_name")
-    price: float = Field(..., alias="price")
+    price: Decimal = Field(..., max_digits=10, decimal_places=2)
     product_id: int = Field(..., alias="product_id")
